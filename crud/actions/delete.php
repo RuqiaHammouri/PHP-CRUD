43f63b8f3184bc0ba1ab348de90a620a0c436eb8
RuqiaHammouri <roqia.hammouri112@gmail.com>
2022-01-17 20:53:03 +0200

    delete and update done

--- crud/actions/delete.php
+++ crud/actions/delete.php
@@ -5,7 +5,7 @@ require_once('../connection/connection.php');
 $connection = OpenConnection();
 
 if(isset($_GET['delete'])){
-    $id = $_GET['delete'];
+    $id = $_GET['id'];
 
 $connection->query("DELETE FORM todo WHERE id=$id")or die($mysqli->error());
 }
--- crud/actions/edit.php
+++ crud/actions/edit.php
@@ -3,3 +3,14 @@
 require_once('../connection/connection.php');
 
 $connection = OpenConnection();
+
+if(isset($_POST['edit'])){
+    $id = $_POST['id'];
+    $title = $_POST['titee'];
+
+$connection->query("UPDATE todo SET  title = '$title' WHERE id = $id")or die($mysqli->error());
+}
+
+$connection->close()
+
+?>
